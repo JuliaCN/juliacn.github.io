48af5a0fe8bac486edffad33033fa07e198c1f42
Jun Tian <tianjun.cpp@gmail.com>
2018-08-29 22:20:56 +0800

    test circleci

---
+++ .circleci/setup-github.sh
@@ -0,0 +1,9 @@
+find . -maxdepth 1 ! -name '_site' ! -name '.git' ! -name '.gitignore' -exec rm -rf {} \;
+mv _site/* .
+rm -R _site/
+
+git add -fA
+git commit --allow-empty -m "$(git log master -1 --pretty=%B)"
+git push origin gh-pages
+
+echo "deployed successfully"
