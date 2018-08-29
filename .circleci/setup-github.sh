292518094ea46181ec0ba78884562fe63a9f2c0d
Jun Tian <tianjun.cpp@gmail.com>
2018-08-29 22:32:18 +0800

    fix error

--- .circleci/setup-github.sh
+++ .circleci/setup-github.sh
@@ -1,3 +1,6 @@
+git config user.name "juliacn"
+git config user.email "no-reply@juliacn.com"
+
 find . -maxdepth 1 ! -name '_site' ! -name '.git' ! -name '.gitignore' -exec rm -rf {} \;
 mv _site/* .
 rm -R _site/
